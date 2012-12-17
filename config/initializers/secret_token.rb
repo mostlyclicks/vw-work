559a31b50413f02a4b26239ceeb0cd9ccf4d26d7
mostlyclicks <carlos@mostlyclicks.com>
2012-12-17 11:50:03 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Visitwinona::Application.config.secret_token = '4768463970175b0b7e54360e03d7e1b4a471405651b18125e9ce44b9024aefa5ff4c3362ddc09c7d0dfdd66367a3fec89b692c071dc9f41adc798f625057fa16'
